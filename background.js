e8f2b4ae2c5830cce091bf0809856a880cbc55cc
Tom <tom.terado@gmail.com>
2019-02-21 16:50:26 +0900

    Basic Functionalities added

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    alert('HELLOOOOO WORLD!!');
-});
+
+function startTimer() {
+    var settingIcon = chrome.browserAction.setIcon({path: "icon2.png"});
+    var addText = chrome.browserAction.setBadgeText({text: "25m"}); // We have 10+ unread items.
+    var t = setTimeout(function(){ alert("Time is up!"); }, 3000);
+    
+    var d = new Date();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    var s = d.getSeconds();
+    if (s == 03){
+        var timerSound = new Audio("sound/boom.wav");
+        timerSound.Play();
+    }
+}
+
+chrome.browserAction.onClicked.addListener(startTimer);
