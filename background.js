7e61881cd395376c1fb5456533c01ee0f0605bc1
Tom <tom.terado@gmail.com>
2019-02-22 16:02:56 +0900

    50% finished

--- background.js
+++ background.js
@@ -1,17 +1,48 @@
+var startTime;
+var i = 10;
+var greenCounter;
 
 function startTimer() {
-    var settingIcon = chrome.browserAction.setIcon({path: "icon2.png"});
-    var addText = chrome.browserAction.setBadgeText({text: "25m"}); // We have 10+ unread items.
-    var t = setTimeout(function(){ alert("Time is up!"); }, 3000);
-    
-    var d = new Date();
-    var m = d.getMinutes();
-    var h = d.getHours();
-    var s = d.getSeconds();
-    if (s == 03){
-        var timerSound = new Audio("sound/boom.wav");
-        timerSound.Play();
-    }
+    changeIcon();
+    tttt;
 }
+
+var countDown = setInterval(function() {
+  chrome.browserAction.setBadgeText({text:String(i) + 's'});
+  if (i <= 0){
+      clearInterval(countDown);
+      playMusic();
+      chrome.browserAction.setBadgeText({text: ''})
+  }
+  i--; 
+}, 1000);
+
+function changeIcon() {
+    var timeleft = 5;
+    startTime = new Date();
+    var s = startTime.getTime()/1000
+    timeString = s.toString();
+    
+    var settingIcon = chrome.browserAction.setIcon({path: "icon2.png"});
+    ///set the alarm...
+    // var playMusic = setTimeout(function(){
+    //     console.log("Time is up!");
+    //     var audio = new Audio("sounds/boom.wav");
+    //     audio.play();
+    // }, 3000);
+    
+}
+
+function playMusic(){
+    var audio = new Audio("sounds/boom.wav");
+    audio.play();
+}
+
+
+// function big(){
+//     var measureT = setInterval(function(){
+//         console.log("hello 1 second");
+//     }, 1000);
+// }
 
 chrome.browserAction.onClicked.addListener(startTimer);
