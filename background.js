b99bce3461ffece3d1a448f95a8e9c6ef3f9c341
Tom <tom.terado@gmail.com>
2019-02-26 13:37:46 +0900

    Finished Working Concept

--- background.js
+++ background.js
@@ -1,51 +1,75 @@
-var startTime;
+
 var i = 5;
-var greenCounter;
+var clickCounter = 0;
+var j = 3;
 
-function startTimer() {
-    changeIcon();
+function startFirstTimer() {
+    var countDown1 = setInterval(function() {
+        chrome.browserAction.setBadgeText({text:String(i) + 's'});
+        if (i <= 0){
+            clearInterval(countDown1);
+            audioPlay();
+            chrome.browserAction.setBadgeText({text: ''})
+            notifyMe();
+            var settingIcon = chrome.browserAction.setIcon({path: "icon2.png"});
+            clickCounter++;
+        }
+        i--;
+      }, 1000);
 }
 
-var countDown = setInterval(function() {
-  chrome.browserAction.setBadgeText({text:String(i) + 's'});
-  if (i <= 0){
-      clearInterval(countDown);
-      playMusic();
-      chrome.browserAction.setBadgeText({text: ''})
-      notifyMe();
-  }
-  i--; 
-}, 1000);
-
-function notifyMe() {
-    chrome.notifications.create('some id for this notification', chromeOptions);
+function startSecondTimer(){
+    var countDown2 = setInterval(function() {
+        chrome.browserAction.setBadgeText({text:String(j) + 's'});
+        if (j <= 0){
+            clearInterval(countDown2);
+            audioPlay();
+            chrome.browserAction.setBadgeText({text: ''})
+            notifyMe2();
+            var settingIcon2 = chrome.browserAction.setIcon({path: "icon.png"});
+            clickCounter++;
+        }
+        j--; 
+      }, 1000);
 }
 
-var chromeOptions = {
-    type: "basic",
-    title: "Test notificaiton.",
-    message: "A Message",
-    iconUrl: "test.png",
-};
-
-function changeIcon() {
-    var timeleft = 5;
-    startTime = new Date();
-    var s = startTime.getTime()/1000
-    timeString = s.toString();
-    
-    var settingIcon = chrome.browserAction.setIcon({path: "icon2.png"});
-    ///set the alarm...
-    // var playMusic = setTimeout(function(){
-    //     console.log("Time is up!");
-    //     var audio = new Audio("sounds/boom.wav");
-    //     audio.play();
-    // }, 3000);
-}
-
-function playMusic(){
+function audioPlay(){
     var audio = new Audio("sounds/boom.wav");
     audio.play();
 }
 
-chrome.browserAction.onClicked.addListener(startTimer);
+function notifyMe() {
+    chrome.notifications.create("", chromeOptions);
+    chrome.notifications.clear("");
+}
+function notifyMe2() {
+    chrome.notifications.create("", chromeOptions2);
+    chrome.notifications.clear("");
+}
+
+var chromeOptions = {
+    type: "basic",
+    title: "Your 25 minutes is up.",
+    message: "Time to take a break",
+    iconUrl: "test.png",
+};
+
+var chromeOptions2 = {
+    type: "basic",
+    title: "Your 5 minutes is up.",
+    message: "Time to work again",
+    iconUrl: "test.png",
+};
+
+chrome.browserAction.onClicked.addListener(function test(){
+    if (clickCounter == 0){
+        startFirstTimer();
+    } else if (clickCounter == 1){
+        startSecondTimer();
+    } else if (clickCounter == 2){
+        clickCounter = 0;
+        i = 5;
+        j = 3;
+        startFirstTimer();
+    }
+});
